2fc18eaae6a7d523664a5e339fdacdd3c13daad4
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-10-18 13:38:36 +0200

    Add onScoreUpdate

--- src/App.js
+++ src/App.js
@@ -19,6 +19,18 @@ class App extends Component {
      	]
    	}
  	}
+
+ 	onScoreUpdate = (playerIndex, scoreChange) => {
+	  this.setState({
+	    players: this.state.players.map((player, index) => {
+	      if (index === playerIndex) {
+	        return { ...player, score: player.score + scoreChange };
+	      }
+	      return player;
+	    })
+	  })
+	}
+
   render() {
     return (
       <div className="App">
