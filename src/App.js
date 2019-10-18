1e41f2ced4e15d5a8389e9992808b1f4c9f80063
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-10-18 11:47:45 +0200

    Add players and import playerlist

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import PlayerList from './components/PlayerList/PlayerList';
 
 class App extends Component {
+  constructor() {
+   	super();
+
+   	this.state = {
+     	players: [
+       	{
+         	name: 'Kunegunda',
+         	score: 5,
+       	},
+       	{
+         	name: 'Antoś',
+         	score: 0,
+       	}
+     	]
+   	}
+ 	}
   render() {
     return (
       <div className="App">
+      	<PlayerList players={this.state.players} />
       </div>
     );
   }
